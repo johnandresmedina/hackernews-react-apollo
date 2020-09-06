4e0d0857a833ff2791da51364324a6c5abea6540
John Medina <jhonandresmedina@gmail.com>
2020-09-05 19:11:09 -0500

    Configuration @apollo/client and graphql

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import Routes from './Routes';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
 const Main = () => {
-  return <Routes />;
+  return (
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
+  );
 };
 
 export default Main;
